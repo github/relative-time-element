0d98a28ba3371a58ac3c8bc33614d9419e7781ae
Joshua Peek <josh@joshpeek.com>
2014-04-22 18:43:14 -0500

    Split up other test cases too

---
+++ test/constructor.js
@@ -0,0 +1,22 @@
+module('constructor');
+
+test('create from document.createElement', function() {
+  var time = document.createElement('local-time');
+  equal('LOCAL-TIME', time.nodeName);
+});
+
+test('create from constructor', function() {
+  var time = new window.LocalTimeElement();
+  equal('LOCAL-TIME', time.nodeName);
+});
+
+test('ignores elements without a datetime attr', function() {
+  var time = document.createElement('local-time');
+  equal(time.textContent, '');
+});
+
+test('leaves contents alone if only datetime is set', function() {
+  var time = document.createElement('local-time');
+  time.setAttribute('datetime', '1970-01-01T00:00:00.000Z');
+  equal(time.textContent, '');
+});
---
+++ test/format.js
@@ -0,0 +1,43 @@
+module('format');
+
+test('null getFormattedDate when datetime missing', function() {
+  var time = document.createElement('local-time');
+  time.setAttribute('format', '%Y-%m-%dT%H:%M:%SZ');
+  equal(time.getFormattedDate(), null);
+});
+
+test('null getFormattedDate when format missing', function() {
+  var time = document.createElement('local-time');
+  time.setAttribute('datetime', '1970-01-01T00:00:00.000Z');
+  equal(time.getFormattedDate(), null);
+});
+
+test('getFormattedDate for datetime and format attributes', function() {
+  var time = document.createElement('local-time');
+  time.setAttribute('datetime', '1970-01-01T00:00:00.000Z');
+  time.setAttribute('format', '%Y-%m-%d');
+  equal(time.getFormattedDate(), window.epochLocalDate);
+});
+
+test('ignores contents if datetime attribute is missing', function() {
+  var time = document.createElement('local-time');
+  time.setAttribute('format', '%Y-%m-%dT%H:%M:%SZ');
+  equal(time.textContent, '');
+});
+
+test('sets formatted contents to format attribute', function() {
+  var time = document.createElement('local-time');
+  time.setAttribute('datetime', '1970-01-01T00:00:00.000Z');
+  time.setAttribute('format', '%Y-%m-%d');
+  equal(time.textContent, window.epochLocalDate);
+});
+
+test('sets formatted contents when parsed element is upgraded', function() {
+  var root = document.createElement('div');
+  root.innerHTML = '<local-time datetime="1970-01-01T00:00:00.000Z" ' +
+    'format="%Y-%m-%d"></local-time>';
+  if ('CustomElements' in window) {
+    window.CustomElements.upgradeSubtree(root);
+  }
+  equal(root.children[0].textContent, window.epochLocalDate);
+});
