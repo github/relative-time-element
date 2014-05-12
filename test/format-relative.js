d756eff6f1818bd40726e83b8445929c4c4dfc91
David Graham <dgraham@github.com>
2014-05-12 15:55:40 -0600

    Fix relative-time formatting tests.

--- test/format-relative.js
+++ test/format-relative.js
@@ -1,41 +1,37 @@
-module('format=relative');
+module('relative-time');
 
 test('getFormattedFromDate for datetime and from=now attributes', function() {
   var minsAgo = new Date(Date.now() - 180000).toISOString();
-  var time = document.createElement('local-time');
+  var time = document.createElement('time', 'relative-time');
   time.setAttribute('datetime', minsAgo);
-  time.setAttribute('format', 'relative');
   equal(time.getFormattedDate(), '3 minutes ago');
 });
 
 test('rewrites from now past datetime to minutes ago', function() {
   var now = new Date(Date.now() - 180000).toISOString();
-  var time = document.createElement('local-time');
+  var time = document.createElement('time', 'relative-time');
   time.setAttribute('datetime', now);
-  time.setAttribute('format', 'relative');
   equal(time.textContent, '3 minutes ago');
 });
 
 test('rewrites a few seconds ago to just now', function() {
   var now = new Date().toISOString();
-  var time = document.createElement('local-time');
+  var time = document.createElement('time', 'relative-time');
   time.setAttribute('datetime', now);
-  time.setAttribute('format', 'relative');
   equal(time.textContent, 'just now');
 });
 
 test('displays future times as just now', function() {
   var now = new Date(Date.now() + 3000).toISOString();
-  var time = document.createElement('local-time');
+  var time = document.createElement('time', 'relative-time');
   time.setAttribute('datetime', now);
-  time.setAttribute('format', 'relative');
   equal(time.textContent, 'just now');
 });
 
 test('sets relative contents when parsed element is upgraded', function() {
   var now = new Date().toISOString();
   var root = document.createElement('div');
-  root.innerHTML = '<local-time datetime="'+now+'" format=relative></local-time>';
+  root.innerHTML = '<time is="relative-time" datetime="'+now+'"></time>';
   if ('CustomElements' in window) {
     window.CustomElements.upgradeSubtree(root);
   }
