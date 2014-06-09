f820b071d37d2d9c84e417501baa351c9b33990f
David Graham <dgraham@github.com>
2014-06-09 10:01:07 -0600

    Slashes.

--- time-elements.js
+++ time-elements.js
@@ -227,12 +227,11 @@
     }
   };
 
-  /* Private: Determine if the day should be formatted before the month name in
-   * the user's current locale. For example, `9 Jun` for en-GB and `Jun 9`
-   * for en-US.
-   *
-   * Returns true if the day appears before the month.
-   */
+  // Private: Determine if the day should be formatted before the month name in
+  // the user's current locale. For example, `9 Jun` for en-GB and `Jun 9`
+  // for en-US.
+  //
+  // Returns true if the day appears before the month.
   function isDayFirst() {
     if (!('Intl' in window)) {
       return false;
@@ -244,11 +243,10 @@
     return !!output.match(/^\d/);
   }
 
-  /* Private: Determine if the year should be separated from the month and day
-   * with a comma. For example, `9 Jun 2014` in en-GB and `Jun 9, 2014` in en-US.
-   *
-   * Returns true if the date needs a separator.
-   */
+  // Private: Determine if the year should be separated from the month and day
+  // with a comma. For example, `9 Jun 2014` in en-GB and `Jun 9, 2014` in en-US.
+  //
+  // Returns true if the date needs a separator.
   function isYearSeparator() {
     if (!('Intl' in window)) {
       return true;
