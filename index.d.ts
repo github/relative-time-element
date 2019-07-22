6fd77914470d9fa0640ece167bea3c9a51a0028c
Kristján Oddsson <koddsson@gmail.com>
2019-07-22 14:04:58 +0100

    add typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+class ExtendedTimeElement extends HTMLElement {
+  get date(): Date | undefined
+  getFormattedTitle(): string | undefined
+  getFormattedDate(): string | undefined
+}
+
+export class LocalTimeElement extends ExtendedTimeElement {
+  getFormattedDate(): string | undefined
+}
+
+export class RelativeTimeElement extends ExtendedTimeElement {
+  getFormattedDate(): string | undefined
+}
+
+export class TimeAgoElement extends ExtendedTimeElement {
+  getFormattedDate(): string | undefined
+}
+
+export class TimeUntilElement extends ExtendedTimeElement {
+  getFormattedDate(): string | undefined
+}
