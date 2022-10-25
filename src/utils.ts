f3b8df519f63379cdad53ca370ac0003a872efaf
Kristján Oddsson <koddsson@gmail.com>
2022-10-25 12:43:44 +0100

    Remove unused utilities
    Co-authored-by: Keith Cirkel <keithamus@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -37,38 +37,6 @@ export function isDayFirst(locale = 'default'): boolean {
   }
 }
 
-let yearSeparator: boolean | null = null
-const yearFormatter = makeFormatter({day: 'numeric', month: 'short', year: 'numeric'})
-
-// Private: Determine if the year should be separated from the month and day
-// with a comma. For example, `9 Jun 2014` in en-GB and `Jun 9, 2014` in en-US.
-//
-// Returns true if the date needs a separator.
-export function isYearSeparator(): boolean {
-  if (yearSeparator !== null) {
-    return yearSeparator
-  }
-
-  const formatter = yearFormatter()
-  if (formatter) {
-    const output = formatter.format(new Date(0))
-    yearSeparator = !!output.match(/\d,/)
-    return yearSeparator
-  } else {
-    return true
-  }
-}
-
-// Private: Determine if the date occurs in the same year as today's date.
-//
-// date - The Date to test.
-//
-// Returns true if it's this year.
-export function isThisYear(date: Date): boolean {
-  const now = new Date()
-  return now.getUTCFullYear() === date.getUTCFullYear()
-}
-
 // Private: Get preferred Intl locale for a target element.
 //
 // Traverses parents until it finds an explicit `lang` other returns "default".
