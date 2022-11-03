bf84818dc5bc2d1ba06494af20a2978a7b1f732f
Keith Cirkel <keithamus@users.noreply.github.com>
2022-11-03 19:41:37 +0000

    add tests against temporal polyfill

--- test/duration.ts
+++ test/duration.ts
@@ -1,7 +1,43 @@
 import {assert} from '@open-wc/testing'
-import {applyDuration, withinDuration} from '../src/duration.ts'
+import {Duration, applyDuration, withinDuration} from '../src/duration.ts'
+import {Temporal} from '@js-temporal/polyfill'
 
 suite('duration', function () {
+
+  suite('Duration class', () => {
+    const tests = new Set([
+      {input: 'P4Y', years: 4},
+      {input: '-P4Y', years: -4},
+      {input: '-P3MT5M', months: -3, minutes: -5},
+      {input: 'P1Y2M3DT4H5M6S', years: 1, months: 2, days: 3, hours: 4, minutes: 5, seconds: 6},
+      {input: 'P5W', weeks: 5},
+      {input: '-P5W', weeks: -5}
+    ])
+
+    const extractValues = x => ({
+      years: x.years || 0,
+      months: x.months || 0,
+      weeks: x.weeks || 0,
+      days: x.days || 0,
+      hours: x.hours || 0,
+      minutes: x.minutes || 0,
+      seconds: x.seconds || 0
+    })
+    for (const {input, ...expected} of tests) {
+      test(`${input} -> from(${JSON.stringify(expected)})`, () => {
+        assert.deepEqual(extractValues(Temporal.Duration.from(input)), extractValues(expected))
+        assert.deepEqual(extractValues(Duration.from(input)), extractValues(expected))
+      })
+    }
+    for (const {input} of tests) {
+      test(`${input} -> abs()`, () => {
+        const temporalAbs = extractValues(Temporal.Duration.from(input).abs())
+        const abs = extractValues(Duration.from(input).abs())
+        assert.deepEqual(temporalAbs, abs)
+      })
+    }
+  })
+
   suite('applyDuration', function () {
     const referenceDate = '2022-10-21T16:48:44.104Z'
     const tests = new Set([
