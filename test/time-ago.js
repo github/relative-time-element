cd93168e053775b5f5700c4f3648914bde27e31f
Keith Cirkel <keithamus@users.noreply.github.com>
2022-11-25 16:42:07 +0000

    change all tests to use async/await

--- test/time-ago.js
+++ test/time-ago.js
@@ -31,96 +31,108 @@ suite('time-ago', function () {
     }
   })
 
-  test('always uses relative dates', function () {
+  test('always uses relative dates', async () => {
     const now = new Date(Date.now() - 10 * 365 * 24 * 60 * 60 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '10 years ago')
   })
 
-  test('rewrites from now past datetime to minutes ago', function () {
+  test('rewrites from now past datetime to minutes ago', async () => {
     const now = new Date(Date.now() - 3 * 60 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '3 minutes ago')
   })
 
-  test('rewrites a few seconds ago to now', function () {
+  test('rewrites a few seconds ago to now', async () => {
     const now = new Date().toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, 'now')
   })
 
-  test('displays future times as now', function () {
+  test('displays future times as now', async () => {
     const now = new Date(Date.now() + 3 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, 'now')
   })
 
-  test('sets relative contents when parsed element is upgraded', function () {
+  test('sets relative contents when parsed element is upgraded', async () => {
     const now = new Date().toISOString()
     const root = document.createElement('div')
     root.innerHTML = `<time-ago datetime="${now}"></time-ago>`
     if ('CustomElements' in window) {
       window.CustomElements.upgradeSubtree(root)
     }
+    await Promise.resolve()
     assert.equal(root.children[0].shadowRoot.textContent, 'now')
   })
 
-  test('rewrites from now past datetime to months ago', function () {
+  test('rewrites from now past datetime to months ago', async () => {
     const now = new Date(Date.now() - 3 * 30 * 24 * 60 * 60 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '3 months ago')
   })
 
-  test('rewrites time-ago datetimes < 18months as "months ago"', function () {
+  test('rewrites time-ago datetimes < 18months as "months ago"', async () => {
     freezeTime(new Date(2020, 0, 1))
     const then = new Date(2018, 9, 1).toISOString()
     const timeElement = document.createElement('time-ago')
     timeElement.setAttribute('datetime', then)
+    await Promise.resolve()
     assert.equal(timeElement.shadowRoot.textContent, '15 months ago')
   })
 
-  test('rewrites time-ago datetimes >= 18 months as "years ago"', function () {
+  test('rewrites time-ago datetimes >= 18 months as "years ago"', async () => {
     freezeTime(new Date(2020, 0, 1))
     const then = new Date(2018, 6, 1).toISOString()
     const timeElement = document.createElement('time-ago')
     timeElement.setAttribute('datetime', then)
+    await Promise.resolve()
     assert.equal(timeElement.shadowRoot.textContent, '2 years ago')
   })
 
-  test('micro formats years', function () {
+  test('micro formats years', async () => {
     const now = new Date(Date.now() - 10 * 365 * 24 * 60 * 60 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
     time.setAttribute('format', 'micro')
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '10y')
   })
 
-  test('micro formats future times', function () {
+  test('micro formats future times', async () => {
     const now = new Date(Date.now() + 3 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
     time.setAttribute('format', 'micro')
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '1m')
   })
 
-  test('micro formats hours', function () {
+  test('micro formats hours', async () => {
     const now = new Date(Date.now() - 60 * 60 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
     time.setAttribute('format', 'micro')
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '1h')
   })
 
-  test('micro formats days', function () {
+  test('micro formats days', async () => {
     const now = new Date(Date.now() - 25 * 60 * 60 * 1000).toISOString()
     const time = document.createElement('time-ago')
     time.setAttribute('datetime', now)
     time.setAttribute('format', 'micro')
+    await Promise.resolve()
     assert.equal(time.shadowRoot.textContent, '1d')
   })
 })
--- test/title-format.js
+++ test/title-format.js
@@ -2,52 +2,59 @@ import {assert} from '@open-wc/testing'
 import '../src/index.ts'
 
 suite('title-format', function () {
-  test('null getFormattedTitle if datetime is missing', function () {
+  test('null getFormattedTitle if datetime is missing', async () => {
     const time = document.createElement('local-time')
     assert.isUndefined(time.getFormattedTitle())
   })
 
-  test('locale-aware getFormattedTitle for datetime value', function () {
+  test('locale-aware getFormattedTitle for datetime value', async () => {
     const time = document.createElement('local-time')
     time.setAttribute('datetime', '1970-01-01T00:00:00.000Z')
     assert.match(time.getFormattedTitle(), /\d{4}/)
   })
 
-  test('skips setting a title attribute if already provided', function () {
+  test('skips setting a title attribute if already provided', async () => {
     const time = document.createElement('local-time')
     time.setAttribute('title', 'does not change')
     time.setAttribute('datetime', '1970-01-01T00:00:00.000Z')
+    await Promise.resolve()
     assert.equal(time.getAttribute('title'), 'does not change')
   })
 
-  test('skips setting a title attribute if datetime is missing', function () {
+  test('skips setting a title attribute if datetime is missing', async () => {
     const time = document.createElement('local-time')
+    await Promise.resolve()
     assert.isNull(time.getAttribute('title'))
   })
 
-  test('sets the title attribute for datetime value', function () {
+  test('sets the title attribute for datetime value', async () => {
     const time = document.createElement('local-time')
     time.setAttribute('datetime', '1970-01-01T00:00:00.000Z')
+    await Promise.resolve()
     assert.match(time.getAttribute('title'), /\d{4}/)
   })
 
-  test('update the title attribute after a datetime value change', function () {
+  test('update the title attribute after a datetime value change', async () => {
     const time = document.createElement('local-time')
     time.setAttribute('datetime', '1970-05-01T00:00:00.000Z')
+    await Promise.resolve()
     assert.match(time.getAttribute('title'), /1970/)
     time.setAttribute('datetime', '1979-05-01T00:00:00.000Z')
+    await Promise.resolve()
     assert.match(time.getAttribute('title'), /1979/)
     time.setAttribute('title', 'custom title')
     time.setAttribute('datetime', '1989-05-01T00:00:00.000Z')
+    await Promise.resolve()
     assert.match(time.getAttribute('title'), /custom title/)
   })
 
-  test('set the title attribute when parsed element is upgraded', function () {
+  test('set the title attribute when parsed element is upgraded', async () => {
     const root = document.createElement('div')
     root.innerHTML = '<local-time datetime="1970-01-01T00:00:00.000Z"></local-time>'
     if ('CustomElements' in window) {
       window.CustomElements.upgradeSubtree(root)
     }
+    await Promise.resolve()
     assert.match(root.children[0].getAttribute('title'), /\d{4}/)
   })
 })
