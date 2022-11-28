081a27046f01b673ad7a398be5e091fe128aa179
Keith Cirkel <keithamus@users.noreply.github.com>
2022-11-28 10:53:28 +0000

    export RelativeTimeElement as default

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ import TimeAgoElement from './time-ago-element-define.js'
 import TimeUntilElement from './time-until-element-define.js'
 
 export {LocalTimeElement, RelativeTimeElement, TimeAgoElement, TimeUntilElement}
+export default RelativeTimeElement
